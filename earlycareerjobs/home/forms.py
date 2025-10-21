cd6091375a6089b4cba3f31a2cf288c334c40fc1
Vondal <dbracey6@gatech.edu>
2025-10-21 13:42:58 -0400

    Removed unnecessary commented code

--- earlycareerjobs/home/forms.py
+++ earlycareerjobs/home/forms.py
@@ -8,7 +8,6 @@ class ProfileForm(forms.ModelForm):
         fields = [
             'headline',
             'skills',
-            #'education',
             'experience',
             'linkedin',
             'github',
@@ -17,7 +16,6 @@ class ProfileForm(forms.ModelForm):
         widgets = {
             'headline': forms.TextInput(attrs={'class': 'form-control'}),
             'skills': forms.Textarea(attrs={'class': 'form-control', 'rows': 4}),
-            #'education': forms.Textarea(attrs={'class': 'form-control', 'rows': 4}),
             'experience': forms.Textarea(attrs={'class': 'form-control', 'rows': 4}),
             'linkedin': forms.URLInput(attrs={'class': 'form-control'}),
             'github': forms.URLInput(attrs={'class': 'form-control'}),
--- earlycareerjobs/home/models.py
+++ earlycareerjobs/home/models.py
@@ -8,7 +8,6 @@ class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='home_profile', related_query_name='home_profile',)
     headline = models.CharField(max_length=255, blank=True)
     skills = models.TextField(blank=True, help_text='Comma-separated skills')
-    #education = models.TextField(blank=True)
     experience = models.TextField(blank=True)
     linkedin = models.URLField(blank=True)
     github = models.URLField(blank=True)
