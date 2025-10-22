40509f7284e90a53bbae6c513d7f03663d2bb173
Yash Pandibabu <ypandibabu3@gatech.edu>
2025-10-22 17:31:51 -0400

    updated changes for privacy options

--- earlycareerjobs/users/admin.py
+++ earlycareerjobs/users/admin.py
@@ -1,18 +1,19 @@
 from django.contrib import admin
-from .models import JobSeekerProfile, ProfilePrivacy
+from .models import CustomUser, ProfilePrivacy
 
 class ProfilePrivacyInline(admin.StackedInline):
     model = ProfilePrivacy
     can_delete = False
 
-@admin.register(JobSeekerProfile)
-class JobSeekerProfileAdmin(admin.ModelAdmin):
-    list_display = ['full_name', 'email', 'location', 'experience_years']
-    search_fields = ['full_name', 'email']
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'role', 'phone_number']
+    search_fields = ['username', 'email']
+    list_filter = ['role']
     inlines = [ProfilePrivacyInline]
 
 @admin.register(ProfilePrivacy)
 class ProfilePrivacyAdmin(admin.ModelAdmin):
-    list_display = ['profile', 'is_profile_visible', 'updated_at']
+    list_display = ['user', 'is_profile_visible', 'updated_at']
     list_filter = ['is_profile_visible', 'updated_at']
-    search_fields = ['profile__full_name']
+    search_fields = ['user__username', 'user__email']
