42f4af7fbf64743465e0557dfbfcaaffc20da041
Cinthya Amaya <camaya8@gatech.edu>
2025-10-25 01:00:25 -0400

    Add inline field editing on job show page

--- earlycareerjobs/jobs/urls.py
+++ earlycareerjobs/jobs/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('new/', views.create_job, name='jobs.create'),
     path('<int:id>/edit/', views.edit_job, name='jobs.edit'),
     path('<int:id>/', views.show, name='jobs.show'),
+    path('<int:id>/update/<str:field>/', views.update_job_field, name='jobs.update_field'),
     path('<int:id>/application/start/', views.start_application, name='jobs.start_application'),
     path('<int:id>/delete/', views.delete_job, name='jobs.delete'),
     path('<int:job_id>/applicant/<int:app_id>/delete/', views.delete_applicant, name='jobs.delete_applicant'),
